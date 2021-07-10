868432b7d24ee79282e98f1f1453d102c162d63e
max180643 <max180643@gmail.com>
2021-07-10 15:58:52 +0700

    :rocket: array

---
+++ array.ts
@@ -0,0 +1,11 @@
+// Array type
+// การประกาศตัวแปรโดยระบุ
+// let sports: Array<string>
+let sports: string[]
+
+sports.push("Football")
+sports.push("Tennis")
+
+// ให้ ts ระบุ type เอง
+const numbers = [1, 2, 3]
+numbers.push(4)
